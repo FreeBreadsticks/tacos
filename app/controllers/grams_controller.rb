cd37ad7c90208e65dbaeadb6175feedb317e6364
Erika <erika.hughes@dbspublishing.com>
2016-05-18 21:10:03 +0000

    Styled all pages

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -60,7 +60,3 @@ private
 def gram_params
   params.require(:gram).permit(:message, :picture)
 end
-
-def render_not_found(status=:not_found)
-  render text: "#{status.to_s.titleize} :(", status: status
-end
